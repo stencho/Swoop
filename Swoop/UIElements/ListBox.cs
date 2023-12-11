8bf94869acbf8cb4e1d7e708a127989d9a8c96c6
Astronat <c3142113@gmail.com>
2023-12-11 13:03:06 +1100

    Added ListBox item selection

--- Swoop/UIElements/ListBox.cs
+++ Swoop/UIElements/ListBox.cs
@@ -16,7 +16,7 @@ namespace SwoopLib.UIElements {
         internal RenderTarget2D render_target => rt.render_target;
 
         int left_margin = 4;
-
+        
         bool _custom_draw = false;
         public bool custom_draw => _custom_draw;
 
@@ -93,8 +93,13 @@ namespace SwoopLib.UIElements {
     public class ListBox : UIElement {
         List<ListBoxItem> items = new List<ListBoxItem>();
 
+        InputHandler handler = new InputHandler();
+
         int top_margin = 5;
         int bottom_margin = 4;
+
+        int selected_index = -1;
+        int mouse_over_index = -1;
 
         float lb_height => (float)size.Y;
         int total_height = 0;
@@ -159,7 +164,7 @@ namespace SwoopLib.UIElements {
 
         internal override void draw_rt() {
             int running_height = 0;
-
+            int index = 0;
 
             foreach (ListBoxItem item in items) {
                 if (item.custom_draw) {
@@ -185,26 +190,63 @@ namespace SwoopLib.UIElements {
 
                     running_height += item.height + top_margin + bottom_margin + 1;
                 }
+                bool stored = (stored_index == index || stored_index == index + 1);
+                bool mouse_on_index = (mouse_over_index == index || mouse_over_index == index + 1);
+                bool selected = (selected_index == index || selected_index == index + 1);
 
-                Drawing.line((Vector2.UnitY * (running_height - scroll_position)), (Vector2.UnitY * (running_height - scroll_position)) + (Vector2.UnitX * size_minus_scroll_bar.X), Swoop.UI_color, 1f);
+                if (stored || mouse_on_index)
+                    Drawing.fill_rect_dither(
+                        (XYPair.UnitY * (running_height - scroll_position)), 
+                        (XYPair.UnitY * (running_height - scroll_position)) + (Vector2.UnitX * size_minus_scroll_bar.X) + (Vector2.UnitY), 
+                        selected || stored ? Swoop.UI_highlight_color : Swoop.UI_color, Swoop.UI_background_color);
+                else 
+                    Drawing.line(
+                        (Vector2.UnitY * (running_height - scroll_position)), 
+                        (Vector2.UnitY * (running_height - scroll_position)) + (Vector2.UnitX * size_minus_scroll_bar.X),
+                        selected_index == index || selected_index == index + 1? Swoop.UI_highlight_color : Swoop.UI_color, 1f);
+                
+                index++;
             }
 
             //draw scroll bar            
             Drawing.fill_rect(
                 size_minus_scroll_bar.X_only.ToVector2() + (size.Y_only.ToVector2() * top_position_fract),
                 size.X_only.ToVector2() + (size.Y_only.ToVector2() * bottom_position_fract),                
-                Swoop.UI_color);
+                Swoop.get_color(this));
 
-            Drawing.line(size_minus_scroll_bar.X_only, size_minus_scroll_bar, Swoop.UI_color, 1f);
+            Drawing.line(size_minus_scroll_bar.X_only, size_minus_scroll_bar, Swoop.get_color(this), 1f);
         }
 
         internal override void handle_focused_input() {
 
         }
 
-        InputHandler handler = new InputHandler();
+        int find_index(float y_position) {
+            int running_height = 0;
+            int index = 0;
+
+            foreach (ListBoxItem item in items) {
+                if (y_position >= running_height && y_position <= running_height + (item.custom_draw ? item.height : item.height + top_margin + bottom_margin + 1)) { //check custom draw and add extra height for margins etc
+                    return index;
+                }
+
+                if (item.custom_draw) {
+                    running_height += item.height;
+                } else {
+                    running_height += item.height + top_margin + bottom_margin + 1;
+                }
+
+                index++;
+            }
+
+            return -1;
+        }
+
+        int stored_index = -1;
+
         internal override void update() {
             handler.update();
+
             if (mouse_over) {
                 var delta = handler.scroll_delta;
                 scroll_position -= delta / 6f;
@@ -215,7 +257,26 @@ namespace SwoopLib.UIElements {
                 if (bottom_position_fract > 1f) {
                     scroll_position = total_height - lb_height;
                 }
+
+                var mouse_pos = scroll_position + mouse_relative.Y;
+                mouse_over_index = find_index(mouse_pos);
+
+                if (clicking && !was_clicking) {
+                    stored_index = mouse_over_index;
+                } else if (!clicking && was_clicking) {
+                    if (stored_index == mouse_over_index)
+                        selected_index = stored_index;
+
+                    stored_index = -1;
+                }
+            } else {
+                if (!clicking && was_clicking) {
+                    stored_index = -1;
+                }
+
+                mouse_over_index = -1;
             }
         }
+
     }
 }
