34a413a669221d189d091fdbbd22fccc1015db56
Astronat <c3142113@gmail.com>
2024-03-08 14:26:14 +1100

    Make GDICanvas unfocusable

--- Swoop/UIElements/GDICanvas.cs
+++ Swoop/UIElements/GDICanvas.cs
@@ -11,13 +11,13 @@ namespace SwoopLib.UIElements {
 
         public GDICanvas(string name, XYPair position, XYPair size, Action<System.Drawing.Graphics> draw_action) : base(name, position, size) {
             gdi_bitmap = new RenderableGDIBitmap(size.X, size.Y, draw_action);
+            can_be_focused = false;
         }
 
         public void update_texture() {
             gdi_bitmap.copy_bitmap_to_texture();
         }
 
-        internal override void added() {}
 
         internal override void draw() {
             gdi_bitmap.update_canvas();
@@ -39,5 +39,6 @@ namespace SwoopLib.UIElements {
 
         internal override void update() {
         }
+        internal override void added() { }
     }
 }
