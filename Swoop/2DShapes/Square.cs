292fe44ac03e9de8dba7c51615ff59835d30efd5
Astronat <c3142113@gmail.com>
2024-07-21 18:35:07 +1000

    might add orientation to GJK square later (if I'm feeling cute)

--- Swoop/2DShapes/Square.cs
+++ Swoop/2DShapes/Square.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,21 @@ namespace SwoopLib.Shapes {
         public Vector2 size { get; set; }
 
         public Color debug_color { get; set; } = Color.White;
-
+        /*
+        float _orientation = 0f;
+        public float orientation { 
+            get { return _orientation; } 
+            set {
+                if (value > 360f) _orientation = value % 360f;
+                else if (value < 0f) {
+                    if (value < -360f) {
+                        var below_zero = float.Abs(value) % 360f;
+                        _orientation = 360f - below_zero;
+                    } else _orientation = 360f - float.Abs(value);
+                } else _orientation = value;
+            } 
+        }
+        */
         public Square(Vector2 position, Vector2 size) {
             this.position = position;
             this.size = size;
